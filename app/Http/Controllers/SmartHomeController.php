80633e670fd11ce32d654f51d21a4ab61b1b09ac
Nugie kurniawan <nugiebinhar@gmail.com>
2025-11-25 22:32:54 +0700

    model smarthome init

---
+++ app/Http/Controllers/SmartHomeController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\SmartHome;
+use Illuminate\Http\Request;
+
+class SmartHomeController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(SmartHome $smartHome)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(SmartHome $smartHome)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, SmartHome $smartHome)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(SmartHome $smartHome)
+    {
+        //
+    }
+}
